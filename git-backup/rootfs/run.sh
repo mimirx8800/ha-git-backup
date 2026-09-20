4261c2f3a5bdd56d1dc236301ab02f47ff98b223
mimirx8800 <mimirx@gmail.com>
2026-09-20 21:47:57 +0200

    Update run.sh
    exclusion update

--- git-backup/rootfs/run.sh
+++ git-backup/rootfs/run.sh
@@ -357,6 +357,7 @@ do_backup() {
     --exclude='esphome/.device-builder-peer-link-key.bin'
     --exclude='esphome/.device-builder.json.lock'
     --exclude='esphome/archive/'
+    --exclude='www/tmp*.jpg'
 )
 
     # Apply user-defined exclusions from add-on configuration
